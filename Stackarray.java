43315d77f96d1fc6f217228ef30663e5ff5abfc1
Gokulhariprasath <gokulhari0127@gmail.com>
2025-03-29 20:16:07 +0530

    Update Stackarray.java

--- Stackarray.java
+++ Stackarray.java
@@ -1,76 +1,74 @@
 import java.util.*;
-class Stackarray
+public class Stackarray
 {
-    private int top;
-    private int st[];
     private int max;
-    Stackarray(int size){
-        top = -1;
+    private int top;
+    private int []st;
+
+    public Stackarray(int size) {
         max = size;
-        st = new int [max];
-    } 
-    public void push(int value){
-        if(top == max-1)
+        st = new int[max];
+        top = -1;
+    }
+    public void push(int v)
+    {
+        if(top ==max - 1)
         {
             System.out.println("Stack overflow");
+            return;
         }
-        else{
-            st[++top]=value;
-            System.out.println(value+"element pushed into stack");
-        }
+        st[++top] = v;
+        System.out.println(v+" is pushed into the stack");
     }
-    public int pop()
+    public void pop()
     {
-        if (top == -1){
-            System.out.println("Stack is Underflow");
-            return-1;
+        if(top == -1)
+        {
+            System.out.println("stack underflow");
+            return;
         }
-        else{
-            int popelement = st[top--];
-            System.out.println("the element poped from stack is"+popelement);
-            return popelement ;
-        }
+        System.out.println(st[top--]+" is popped out of the stack");
     }
     public void display()
     {
-         System.out.println("Stack element is printing");
-       for(int i =top;i>=0;i--)
-       {
-       
-        System.out.println(st[i]);
-       }
-    }
-    public static void main (String args[])
-    {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Enter your limit:");
-        int n = sc.nextInt();
-        Stackarray s = new Stackarray(n);
-        System.out.print("1.PUSH 2.POP 3.DISPLAY 4.EXIT");
-        while(true)
+        if(top == -1)
         {
-            System.out.println("Enter your choice:");
-            int c =sc.nextInt();
-        switch(c)
-        {
-            case 1:
-                System.out.println("enter the element push into Stack:");
-                int value = sc.nextInt();
-                s.push(value);
-                break;
-            case 2:
-                s.pop();
-                break;
-            case 3:
-                s.display();
-                break;
-            case 4:
-                sc.close();
-                return;
-                default:
-                    System.out.println("Invalid chice try again");
+            System.out.println("stack is underflow");
+            return;
         }
+        System.out.println("printing element in stack.....");
+        for (int i = top;i>=0;i--) {
+            System.out.println(st[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter limit of stack :");
+        int n = sc.nextInt();
+        Stackarray stk = new Stackarray(n);
+        System.out.println("1.PUSH 2.POP 3.DISPLAY 4.EXIT");
+        while (true) { 
+            System.out.println("Enter your choice :");
+            int c = sc.nextInt();
+            switch(c){
+                case 1:
+                    System.out.println("Enter a value to push into stack :");
+                    int v = sc.nextInt();
+                    stk.push(v);
+                    break;
+                case 2:
+                    stk.pop();
+                    break;
+                case 3:
+                    stk.display();
+                    break;
+                case 4:
+                    System.out.println("Exiting......");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Please enter a valid choice");
+            }
         }
     }
 }
